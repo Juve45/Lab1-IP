b5a01d46d7433e4b8202dba1c13453f29ef2539e
Unknown <juve45@rocketmail.com>
2017-02-26 20:42:45 +0200

    Am pus totul in Main.java

--- Main.java
+++ Main.java
@@ -82,6 +82,45 @@ class Avion extends Aeronava{
 
 }
 
+class Locatie
+{
+    protected ArrayList <Aeronava> listaAeronave;
+
+    Locatie() {
+        listaAeronave = new ArrayList<Aeronava>();
+    }
+
+    public void addAeronava(Aeronava a)
+    {
+        listaAeronave.add(a);
+    }
+}
+
+class Hangar extends Locatie
+{
+    int capacitate;
+
+    @Override
+    public void addAeronava(Aeronava a)
+    {
+        if(this.listaAeronave.size() == capacitate)
+        {
+            System.err.println("Nu mai e loc in hangar!");
+        }
+    }
+}
+
+class Pista extends Locatie
+{
+    int lungime;
+
+}
+
+class Aeroport{
+    private List<Locatie> listaLocatii;
+
+
+}
 
 public class Main {
     
